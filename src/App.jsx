08db329f15bef1de6f9de89a879b60a3b5914fa4
Shubham vishwakarma <subbhamk@gmail.com>
2025-05-08 11:15:10 +0530

    deploing in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/home/home';
 import Profile from './pages/about/profile';
 import Impact from './pages/Impact/impact';
 import Navbar from './components/navbar/navbar';
+ 
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/history" element={<History />} />
         <Route path="/impact" element={<Impact />} />
+       
       </Routes>
     </Router>
   );
