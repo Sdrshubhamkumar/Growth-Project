e73db44e2531f6208c183e3294377fd48665c0e5
Shubham vishwakarma <subbhamk@gmail.com>
2025-05-12 22:13:31 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // ✅ Add Tailwind plugin
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(), // ✅ Include Tailwind
+    react()
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
