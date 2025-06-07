da1e956d109843285073ab8e72eba8268bdbd99a
Shalini-5 <shalininanjegowda2003@gmail.com>
2025-06-07 12:28:24 +0530

    created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Profile from './pages/about/profile';
 import Impact from './pages/Impact/impact';
-import Innovations from './pages/innovations/innovations';
 import Navbar from './components/Navbar/Navbar';
 import History from './pages/about/history';
 import BoardOfTrustees from './pages/about/boardOfTrustees';
 import Governance from './pages/about/governance';
 import SaathInstitutionalPartners from './pages/about/saathInstitutionalPartners';
 import FundersAndPartners from './pages/fundersAndPartners/fundersAndPartners';
+import Publications from './pages/Resources/Publications';
+import AnnualReports from './pages/Resources/annualReports';
+
 
  
 
@@ -24,9 +26,10 @@ function App() {
         <Route path="/governance" element={<Governance />} />
         <Route path="/saath-institutional-partners" element={<SaathInstitutionalPartners />} />
         <Route path="/Funders&Partners" element={<FundersAndPartners />} />
-        <Route path="/innovations" element={<Innovations />} />
+        <Route path="/Publications" element={<Publications />} />
+        <Route path="/Annual-reports" element={<AnnualReports />} />
         <Route path="/impact" element={<Impact />} />
-       
+        
       </Routes>
     </Router>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import close from '../../assets/close.png';
 import ListItem from './nav-list-item/navListItem';
 import MobileListItem from './nav-list-item/navListIItem(mobile)';
 
-function Navbar() { 
+function Navbar() {
   return ( 
     <div className="nav-container sticky top-0 z-50"> 
       {/* Upper Navigation */} 
@@ -51,7 +51,7 @@ function Navbar() {
           <ListItem text="Funders&Partners" /> 
           <ListItem text="Sectors" children={['Education', 'Health', 'LivelyHoods', 'Urban Governance']} /> 
           <ListItem text="Awards" /> 
-          <ListItem text="Resources" children={['Annual Reports', 'Newsletter', 'Intern Reports']} /> 
+          <ListItem text="Resources" children={['Annual Reports', 'Publications', 'Intern Reports']} /> 
           <ListItem text="Get Involved" /> 
           <ListItem text="CSR" /> 
           <ListItem text="Innovations" /> 
@@ -96,8 +96,8 @@ function Navbar() {
               <MobileListItem 
                 text="Resources" 
                 children={[ 
-                  { label: 'Annual Reports', path: '/annual-reports' }, 
-                  { label: 'Newsletter', path: '/newsletter' }, 
+                  { label: 'Annual Reports', path: '/Annual-reports' }, 
+                  { label: 'Publications', path: '/Publications' }, 
                   { label: 'Intern Reports', path: '/intern-reports' }, 
                 ]} 
               /> 
