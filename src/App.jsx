3b55c705fee7dd68eaef2781a829ff9dbab86381
Shubham vishwakarma <subbhamk@gmail.com>
2025-06-08 12:15:30 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Profile from './pages/about/profile';
 import Impact from './pages/Impact/impact';
-import Navbar from './components/Navbar/Navbar'; // Use consistent capitalization
-
-// About pages
+import Innovations from './pages/innovations/innovations';
+import Navbar from './components/Navbar/Navbar';
 import History from './pages/about/history';
 import BoardOfTrustees from './pages/about/boardOfTrustees';
 import Governance from './pages/about/governance';
 import SaathInstitutionalPartners from './pages/about/saathInstitutionalPartners';
-import FundersAndPartners from './pages/FundersAndPartners/funders&Partners';
+import FundersAndPartners from './pages/FundersAndPartners/fundersAndPartners';
  
 
 function App() {
@@ -23,14 +22,10 @@ function App() {
         <Route path="/board-of-trustees" element={<BoardOfTrustees />} />
         <Route path="/governance" element={<Governance />} />
         <Route path="/saath-institutional-partners" element={<SaathInstitutionalPartners />} />
-        <Route path="/csr" element={<CSR />} />
-        <Route path="/funders-and-partners" element={<FundersAndPartners />} />
+        <Route path="/Funders&Partners" element={<FundersAndPartners />} />
         <Route path="/innovations" element={<Innovations />} />
         <Route path="/impact" element={<Impact />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/health" element={<Health />} />
-        <Route path="/livelyhood" element={<LivelyHood />} />
-        <Route path="/urbangovernance" element={<UrbanGovernance />} />
+       
       </Routes>
     </Router>
   );
