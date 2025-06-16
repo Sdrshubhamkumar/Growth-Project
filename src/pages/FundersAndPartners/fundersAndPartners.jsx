6299a48e0eacad7bdc0dff0265e1a7b944ffbfb4
Shalini-5 <shalininanjegowda2003@gmail.com>
2025-06-16 16:50:54 +0530

    updated

--- src/pages/FundersAndPartners/fundersAndPartners.jsx
+++ src/pages/FundersAndPartners/fundersAndPartners.jsx
@@ -68,7 +68,7 @@ function FundersAndPartners() {
                 </div>
 
         <div className="mt-6">
-        <div className="grid grid-cols-6 gap-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
 
           {/* Sector 1 */}
           
